34d7a7843593e97418fa805236d9dd33ada64c59
R-Jyothi <112076734+R-Jyothi@users.noreply.github.com>
2022-08-26 14:51:23 +0530

    first commit

---
+++ Models/Employee.cs
@@ -0,0 +1,12 @@
+﻿namespace Core6.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Mob { get; set; }
+
+        public double salary { get; set; }
+
+    }
+}
---
+++ Models/HRContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Core6.Models
+{
+    public class HRContext :DbContext
+    {
+        public HRContext(DbContextOptions<HRContext> option): base(option)
+        {
+
+        }
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
